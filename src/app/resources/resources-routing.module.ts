bb190fb328b5e61312b4eb5129d44b524f4ac5f6
Richard Leung Woo Gabriel <leungwoo@gmail.com>
2022-03-01 13:07:25 -0400

    lazyloading

--- src/app/resources/resources-routing.module.ts
+++ src/app/resources/resources-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [{ path: '', component: ResourcesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ResourcesRoutingModule { }
+export class ResourcesRoutingModule {}
